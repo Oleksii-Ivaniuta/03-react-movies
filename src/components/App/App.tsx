0c8297a4b42aa77bc85c2361bd1d6000555512c9
Oleksii Ivaniuta <11.ivaniuta@gmail.com>
2025-05-20 10:37:29 +0100

    add searchbar moviegrig

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import fetchMovies from "../../services/movieService";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
@@ -5,17 +6,25 @@ import MovieGrid from "../MovieGrid/MovieGrid";
 import MovieModal from "../MovieModal/MovieModal";
 import SearchBar from "../SearchBar/SearchBar";
 import css from "./App.module.css";
+import type { Movie } from "../../types/movie";
 
 
 export default function App() {
-
-    fetchMovies('batman');
-
-    return (<div className={css.app}>
-        <SearchBar />
-        <ErrorMessage />
-        <Loader />
-        <MovieGrid />
-        <MovieModal />
-    </div>);
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const searchMovie = async (value: string) => {
+        const newMovies = await fetchMovies(value) as Movie[];
+        setMovies(newMovies);
+    }
+    const selectMovie = (id: number): void => {
+        console.log(`select movie ${id}`);
+    }
+    return (
+        <div className={css.app}>
+        <SearchBar onSubmit={searchMovie}/>
+      <ErrorMessage />
+      <Loader />
+      <MovieGrid movies={movies} onSelect={selectMovie}  />
+      <MovieModal />
+    </div>
+  );
 }
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,36 +1,50 @@
 import css from "./SearchBar.module.css";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 
+interface SearchBarProps {
+  onSubmit: (value: string) => void;
+}
 
-export default function SearchBar() {
-    return (
-        <>
-            <header className={css.header}>
-  <div className={css.container}>
-    <a
-      className={css.link}
-      href="https://www.themoviedb.org/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Powered by TMDB
-    </a>
-    <form className={css.form}>
-      <input
-        className={css.input}
-        type="text"
-        name="query"
-        autoComplete="off"
-        placeholder="Search movies..."
-        autoFocus
+export default function SearchBar({ onSubmit }: SearchBarProps) {
+  const handleSubmit = (formData: FormData) => {
+    const filmQuery = formData.get('query') as string;
+    if (!filmQuery) {
+      return toast.error('Please enter your search query.')
+    }
+    onSubmit(filmQuery);
+  }
+
+  return (
+    <>
+                <Toaster
+        position="top-center"
+        reverseOrder={false}
       />
-      <button className={css.button} type="submit">
-        Search
-      </button>
-    </form>
-  </div>
-</header>
+      <header className={css.header}>
+        <div className={css.container}>
+          <a
+            className={css.link}
+            href="https://www.themoviedb.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by TMDB
+          </a>
+          <form action={handleSubmit} className={css.form}>
+            <input
+              className={css.input}
+              type="text"
+              name="query"
+              autoComplete="off"
+              placeholder="Search movies..."
+              autoFocus
+            />
+            <button className={css.button} type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </header>
     </>
-    )
- 
+  );
 }
